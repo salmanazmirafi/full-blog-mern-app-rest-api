7ea3eb527c430f258fe8021891ad474a23a0b37a
Salman Azmi Rafi <salmanazmirafipersonal@gmail.com>
2022-11-07 13:28:51 +0600

    change databse

--- server/controlar/registration.js
+++ server/controlar/registration.js
@@ -1,7 +1,9 @@
 const User = require("../modules/userModel");
+const bcrypt = require("bcrypt");
 // signup
 exports.signup = async (req, res, next) => {
   try {
+    req.body.password = await bcrypt.hash(req.body.password, 11);
     const { username, email, password, profile, coverpicture } = req.body;
     const user = await User.create({
       username,
@@ -14,7 +16,11 @@ exports.signup = async (req, res, next) => {
       message: "Congratulations! Your Accounts Successful Create",
       user,
     });
-  } catch (error) {}
+  } catch (error) {
+    res.status(401).json({
+      message: "Somthisg went wong",
+    });
+  }
 };
 // login
 exports.login = async (req, res) => {
